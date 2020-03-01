619c3fd091cc6f8b94c47e203aa683032bfbee7e
Dave Mackey <dave@davemackey.net>
2020-03-01 18:45:05 -0500

    Create a list containing the contents of source file.

--- pybasictools.py
+++ pybasictools.py
@@ -1,6 +1,11 @@
 # Python BASIC Tools
 
+
+baslines = []
+
 # Open File and Print Contents
 with open('/workspaces/python/pyBASICTools/CWSTRAT.BAS', 'rt') as basfile:
-    bascontents = basfile.read()
-    print(bascontents)
+    for basline in basfile:
+        baslines.append(basline)
+    
+    print(baslines)
